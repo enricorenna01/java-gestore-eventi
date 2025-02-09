52266a839af4bf7856018ef42b6d7f07ac64b5f4
enricorenna01 <erenna39@gmail.com>
2025-02-09 22:27:38 +0100

    feat: added methods

--- src/ProgrammaEventi.java
+++ src/ProgrammaEventi.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,4 +13,41 @@ public class ProgrammaEventi {
         this.eventi = new ArrayList<>();
     }
 
+    //Metodo che aggiunge alla lista un Evento, passato come parametro
+    public void aggiungiEvento(Evento evento) {
+        eventi.add(evento);
+    }
+
+    //Metodo che restituisce una lista con tutti gli eventi presenti in una certa data
+    public List<Evento> eventiInData(LocalDate data) {
+        List<Evento> eventiInData = new ArrayList<>();
+        for(Evento e : eventi) {
+            if (e.getData().equals(data)) {
+                eventiInData.add(e);
+            }
+        }
+        return eventiInData;
+    }
+
+    //Metodo che restituisce quanti eventi sono presenti nel programma
+    public int getNumeroEventi() {
+        return eventi.size();
+    }
+
+    //Metodo che svuota la lista di eventi
+    public void svuotaLista(){
+        eventi.clear();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(titolo).append("\n");
+        eventi.sort((e1, e2) -> e1.getData().compareTo(e2.getData()));
+        for (Evento e : eventi){
+            sb.append(e.toString()).append("\n");
+        }
+        
+        return sb.toString();
+    }
 }
